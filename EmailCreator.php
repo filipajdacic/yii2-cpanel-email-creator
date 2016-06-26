df611f66b539934cedd5f9fa5a792c7690723407
digital-mac <ajdasoft@gmail.com>
2016-06-26 12:47:58 +0200

    Last changes to class

--- EmailCreator.php
+++ EmailCreator.php
@@ -5,6 +5,8 @@ namespace filipajdacic\cpanelemailcreator;
 use yii\base\Component;
 use \Exception;
 
+use vendor\xmlapi;
+
 /**
  * This is just an example.
  */
@@ -38,8 +40,42 @@ class EmailCreator extends Component
 
 
 
+    public function init()
+    {
+        parent::init();
+
+		$this->ip = $ip;
+		$this->cpanel_username = $cpanel_username;
+		$this->cpanel_password = $cpanel_password;
+		$this->port = $port;
+    }
 
 
+	protected function __initApi() {
+		$xmlapi = new xmlapi($this->ip);
+		$xmlapi->set_port($this->port);
+		$xmlapi->password_auth($this->cpanel_username, $this->cpanel_password);
+
+		return $xmlapi;
+	}
+
+
+	public function createNewAccount($domain, $username, $password, $quota) {
+		$api = $this->__initApi();
+		$call = array(
+			'domain' => $domain, 
+			'email' => $username, 
+			'password' => $password, 
+			'quota' => $quota
+		);
+
+		$result = $api->api2_query($this->cpanel_username, "Email", "addpop", $call); 
+		if($result->data->result == 1) {
+			return true;
+		} else {
+			throw new CHttpException(404, 'E-mail account is not created: '.$result->data->reason);
+		}
+	}
 
 
 }
